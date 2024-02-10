9983fcff6fe1e925fe691e12c3a289365117b74e
_ <tacticsrealize2@gmail.com>
2024-02-10 23:40:51 +0900

    change: use: com.github.h0tk3y.betterParse:better-parse, org.jetbrains.kotlinx:kotlinx-coroutines-core

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,12 @@ repositories {
 
 kotlin {
     jvm()
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("com.github.h0tk3y.betterParse:better-parse:0.4.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+            }
+        }
+    }
 }
