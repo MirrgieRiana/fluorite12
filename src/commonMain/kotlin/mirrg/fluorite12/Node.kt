b319d73bdbb47f5acc795ea5fb0d0788d05b88e6
_ <tacticsrealize2@gmail.com>
2024-02-10 23:42:41 +0900

    internal: add: Node

---
+++ src/commonMain/kotlin/mirrg/fluorite12/Node.kt
@@ -0,0 +1,13 @@
+package mirrg.fluorite12
+
+import com.github.h0tk3y.betterParse.lexer.TokenMatch
+
+sealed class Node
+class IdentifierNode(val main: TokenMatch) : Node()
+class NumberNode(val main: TokenMatch) : Node()
+class BracketNode(val left: TokenMatch, val main: Node, val right: TokenMatch) : Node()
+class RightBracketNode(val main: Node, val left: TokenMatch, val argument: Node, val right: TokenMatch) : Node()
+class InfixNode(val left: Node, val operator: TokenMatch, val right: Node) : Node()
+class ComparisonNode(val nodes: List<Node>, val operators: List<TokenMatch>) : Node()
+class ConditionNode(val condition: Node, val question: TokenMatch, val ok: Node, val colon: TokenMatch, val ng: Node) : Node()
+class ListNode(val nodes: List<Node>, val operators: List<TokenMatch>) : Node()
