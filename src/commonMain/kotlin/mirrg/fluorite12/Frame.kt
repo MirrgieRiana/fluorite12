a502d44b19a19a27321c633ad8d32f0fb71564f6
_ <tacticsrealize2@gmail.com>
2024-02-12 08:34:17 +0900

    internal: add: Frame, Objects

---
+++ src/commonMain/kotlin/mirrg/fluorite12/Frame.kt
@@ -0,0 +1,157 @@
+package mirrg.fluorite12
+
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
+
+class Frame(val parent: Frame? = null) {
+    val variables = mutableMapOf<String, Any?>()
+}
+
+suspend fun Frame.evaluate(node: Node): Any? {
+    return when (node) {
+        is IdentifierNode -> {
+            val variable = node.main.text
+            fun f(frame: Frame): Any? {
+                return if (variable in frame.variables) {
+                    frame.variables[variable]
+                } else if (frame.parent != null) {
+                    f(frame.parent)
+                } else {
+                    throw IllegalArgumentException("Unknown variable: $variable")
+                }
+            }
+            f(this)
+        }
+
+        is NumberNode -> node.main.text.toInt()
+
+        is BracketNode -> when (node.left.text) {
+            "(" -> evaluate(node.main)
+
+            "[" -> {
+                val value = evaluate(node.main)
+                if (value is FluoriteStream) {
+                    FluoriteArray(value.flow.toList())
+                } else {
+                    FluoriteArray(listOf(value))
+                }
+            }
+
+            else -> throw IllegalArgumentException("Unknown operator: ${node.left.text} A ${node.right.text}")
+        }
+
+        is RightBracketNode -> when (node.left.text) {
+            "(" -> {
+                val mainValue = evaluate(node.main)
+                require(mainValue is FluoriteFunction)
+                val argumentNodes = if (node.argument is ListNode && node.argument.operators.first().text == ";") {
+                    node.argument.nodes
+                } else {
+                    listOf(node.argument)
+                }
+                val argumentValues = argumentNodes.map { evaluate(it) }
+                mainValue.function(argumentValues)
+            }
+
+            else -> throw IllegalArgumentException("Unknown operator: A ${node.left.text} B ${node.right.text}")
+        }
+
+        is InfixNode -> when (node.operator.text) {
+            "+" -> (evaluate(node.left) as Number).toDouble() + (evaluate(node.right) as Number).toDouble()
+            "-" -> (evaluate(node.left) as Number).toDouble() - (evaluate(node.right) as Number).toDouble()
+            "*" -> (evaluate(node.left) as Number).toDouble() * (evaluate(node.right) as Number).toDouble()
+            "/" -> (evaluate(node.left) as Number).toDouble() / (evaluate(node.right) as Number).toDouble()
+            ".." -> FluoriteStream(((evaluate(node.left) as Number).toInt()..(evaluate(node.right) as Number).toInt()).asFlow())
+
+            "->" -> {
+                val commasNode = if (node.left is BracketNode && node.left.left.text == "(") {
+                    node.left.main
+                } else {
+                    node.left
+                }
+                val identifierNodes = if (commasNode is ListNode && commasNode.operators.first().text == ",") {
+                    commasNode.nodes
+                } else if (commasNode is ListNode && commasNode.operators.first().text == ";") {
+                    commasNode.nodes
+                } else {
+                    listOf(commasNode)
+                }
+                val variables = identifierNodes.map {
+                    require(it is IdentifierNode)
+                    it.main.text
+                }
+                FluoriteFunction { arguments ->
+                    val frame = Frame(this)
+                    frame.variables["__"] = FluoriteArray(arguments)
+                    variables.forEachIndexed { i, it ->
+                        frame.variables[it] = arguments.getOrNull(i)
+                    }
+                    frame.evaluate(node.right)
+                }
+            }
+
+            "|" -> {
+                val stream = evaluate(node.left)
+                val (variable, body) = if (node.right is InfixNode && node.right.operator.text == "=>") {
+                    require(node.right.left is IdentifierNode)
+                    Pair(node.right.left.main.text, node.right.right)
+                } else {
+                    Pair("_", node.right)
+                }
+                suspend fun f(value: Any?): Any? {
+                    val frame = Frame(this)
+                    frame.variables[variable] = value
+                    return frame.evaluate(body)
+                }
+                return if (stream is FluoriteStream) {
+                    FluoriteStream(flow {
+                        stream.flow.collect {
+                            val value = f(it)
+                            if (value is FluoriteStream) {
+                                emitAll(value.flow)
+                            } else {
+                                emit(value)
+                            }
+                        }
+                    })
+                } else {
+                    f(stream)
+                }
+            }
+
+            else -> throw IllegalArgumentException("Unknown operator: A ${node.operator.text} B")
+        }
+
+        is ComparisonNode -> run {
+            val values = arrayOfNulls<Any?>(node.nodes.size)
+            values[0] = evaluate(node.nodes[0])
+            node.operators.forEachIndexed { i, operator ->
+                val leftValue = values[i]
+                val rightValue = evaluate(node.nodes[i + 1])
+                values[i + 1] = rightValue
+                val result = when (operator.text) {
+                    "==" -> leftValue == rightValue
+                    "!=" -> leftValue != rightValue
+                    ">" -> (leftValue as Number).toDouble() > (rightValue as Number).toDouble()
+                    "<" -> (leftValue as Number).toDouble() < (rightValue as Number).toDouble()
+                    ">=" -> (leftValue as Number).toDouble() >= (rightValue as Number).toDouble()
+                    "<=" -> (leftValue as Number).toDouble() <= (rightValue as Number).toDouble()
+                    else -> throw IllegalArgumentException("Unknown operator: A ${operator.text} B")
+                }
+                if (!result) return@run false
+            }
+            true
+        }
+
+        is ConditionNode -> if (evaluate(node.condition) as Boolean) evaluate(node.ok) else evaluate(node.ng)
+
+        is ListNode -> {
+            node.nodes.map {
+                val value = evaluate(it)
+                if (value is FluoriteStream) value else streamOf(value)
+            }.concat()
+        }
+    }
+}
---
+++ src/commonMain/kotlin/mirrg/fluorite12/Objects.kt
@@ -0,0 +1,26 @@
+package mirrg.fluorite12
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.merge
+
+class FluoriteArray(val values: List<Any?>) {
+    override fun toString() = "[${values.joinToString(",") { "$it" }}]"
+}
+
+class FluoriteFunction(val function: suspend (List<Any?>) -> Any?)
+
+
+class FluoriteStream(val flow: Flow<Any?>)
+
+fun streamOf(value: Any?) = FluoriteStream(flowOf(value))
+
+operator fun FluoriteStream.plus(other: FluoriteStream) = FluoriteStream(merge(this.flow, other.flow))
+
+fun Iterable<FluoriteStream>.concat() = FluoriteStream(flow {
+    this@concat.forEach {
+        emitAll(it.flow)
+    }
+})
