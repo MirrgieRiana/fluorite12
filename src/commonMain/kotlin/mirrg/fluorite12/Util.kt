b124208efe6e84ddfa109d014fcb4dcb52fdcf5d
_ <tacticsrealize2@gmail.com>
2024-02-13 04:35:21 +0900

    internal: add: OrCombinator

--- src/commonMain/kotlin/mirrg/fluorite12/Util.kt
+++ src/commonMain/kotlin/mirrg/fluorite12/Util.kt
@@ -1,5 +1,9 @@
 package mirrg.fluorite12
 
+import com.github.h0tk3y.betterParse.combinators.OrCombinator
 import com.github.h0tk3y.betterParse.lexer.TokenMatch
+import com.github.h0tk3y.betterParse.parser.Parser
 
 val List<TokenMatch>.text get() = this.joinToString("") { it.text }
+
+fun <T> OrCombinator(vararg parsers: Parser<T>) = OrCombinator(parsers.toList())
