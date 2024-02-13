2b36a8643ecd972ef0f0dc40ede1ac9ee25bf137
_ <tacticsrealize2@gmail.com>
2024-02-13 23:12:26 +0900

    test: add: stringTest

--- src/commonTest/kotlin/mirrg/fluorite12/Fluorite12Test.kt
+++ src/commonTest/kotlin/mirrg/fluorite12/Fluorite12Test.kt
@@ -2,12 +2,21 @@ package mirrg.fluorite12
 
 import com.github.h0tk3y.betterParse.grammar.tryParseToEnd
 import com.github.h0tk3y.betterParse.parser.Parsed
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class Fluorite12Test {
+    @Test
+    fun stringTest() = runTest {
+        assertTrue((run(""" "abcABC123" """) as String) == "abcABC123")
+        assertTrue((run(""" "" """) as String) == "")
+        assertTrue((run(""" "abc$( "ABC" )123" """) as String) == "abcABC123")
+    }
+
     @Test
     fun test() = runTest {
         assertTrue(run("a->a") is FluoriteFunction)
