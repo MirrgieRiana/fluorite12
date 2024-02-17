50cbceaa452ccbda9d8563af99c5e9b281ce4c26
_ <tacticsrealize2@gmail.com>
2024-02-17 21:36:23 +0900

    FIX: minus

--- src/commonMain/kotlin/mirrg/fluorite12/Grammar.kt
+++ src/commonMain/kotlin/mirrg/fluorite12/Grammar.kt
@@ -190,7 +190,7 @@ class Fluorite12Grammar : Grammar<Node>() {
     val left: Parser<Node> by zeroOrMore(leftOperator) * right map { it.t1.foldRight(it.t2) { f, node -> f(node) } }
 
     val mul: Parser<Node> by leftAssociative(left, -s * (+asterisk or +slash) * -b, ::infixNode)
-    val add: Parser<Node> by leftAssociative(mul, -s * (+plus or +minus) * -b, ::infixNode)
+    val add: Parser<Node> by leftAssociative(mul, -s * (+plus or +(minus * -NotParser(greater))) * -b, ::infixNode)
     val range: Parser<Node> by leftAssociative(add, -s * +(period * period) * -b, ::infixNode)
     val comparison: Parser<Node> by range * zeroOrMore(-s * (+(equal * equal) or +(exclamation * equal) or +greater or +(greater * equal) or +less or +(less * equal)) * -b * range) map {
         if (it.t2.isNotEmpty()) {
