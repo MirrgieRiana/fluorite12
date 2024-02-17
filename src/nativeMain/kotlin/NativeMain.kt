1b4a9f56eab75e67d3b409f31d2c27f52bdbf9db
_ <tacticsrealize2@gmail.com>
2024-02-17 21:31:42 +0900

    ADD: NativeMain: パースエラー時に詳細を表示

--- src/nativeMain/kotlin/NativeMain.kt
+++ src/nativeMain/kotlin/NativeMain.kt
@@ -1,6 +1,5 @@
 import com.github.h0tk3y.betterParse.grammar.tryParseToEnd
-import com.github.h0tk3y.betterParse.parser.ErrorResult
-import com.github.h0tk3y.betterParse.parser.Parsed
+import com.github.h0tk3y.betterParse.parser.toParsedOrThrow
 import kotlinx.coroutines.runBlocking
 import mirrg.fluorite12.Fluorite12Grammar
 import mirrg.fluorite12.FluoriteStream
@@ -9,24 +8,17 @@ import mirrg.fluorite12.evaluate
 
 fun main(args: Array<String>) = runBlocking {
     val src = args[0]
-    when (val parseResult = Fluorite12Grammar().tryParseToEnd(src)) {
-        is Parsed -> {
-            val node = parseResult.value
-            when (val result = Frame().evaluate(node)) {
-                is FluoriteStream -> {
-                    result.flow.collect {
-                        println(it.toString())
-                    }
-                }
-
-                else -> {
-                    println(result.toString())
-                }
+    val parseResult = Fluorite12Grammar().tryParseToEnd(src).toParsedOrThrow()
+    val node = parseResult.value
+    when (val result = Frame().evaluate(node)) {
+        is FluoriteStream -> {
+            result.flow.collect {
+                println(it.toString())
             }
         }
 
-        is ErrorResult -> {
-            println(parseResult.toString())
+        else -> {
+            println(result.toString())
         }
     }
 }
