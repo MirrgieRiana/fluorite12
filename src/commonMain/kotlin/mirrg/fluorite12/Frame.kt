b45dcf171913dc1ff3f9a2e3a4c62dab7d93d8cb
_ <tacticsrealize2@gmail.com>
2024-02-21 22:56:53 +0900

    ADD: empty object

--- src/commonMain/kotlin/mirrg/fluorite12/Frame.kt
+++ src/commonMain/kotlin/mirrg/fluorite12/Frame.kt
@@ -166,10 +166,10 @@ suspend fun Frame.evaluate(node: Node): FluoriteValue {
             }
 
             "{" -> {
-                val nodes = if (node.main is SemicolonNode) {
-                    node.main.nodes
-                } else {
-                    listOf(node.main)
+                val nodes = when (node.main) {
+                    is EmptyNode -> listOf()
+                    is SemicolonNode -> node.main.nodes
+                    else -> listOf(node.main)
                 }
                 val values = mutableMapOf<String, FluoriteValue>()
                 nodes.forEach {
--- src/commonTest/kotlin/mirrg/fluorite12/Fluorite12Test.kt
+++ src/commonTest/kotlin/mirrg/fluorite12/Fluorite12Test.kt
@@ -106,6 +106,7 @@ class Fluorite12Test {
         assertEquals("{a:1;b:2}", run("{a: 1; b: 2}").obj) // エントリーは ; で区切ることができる
         assertEquals("{a:1;b:2}", run("{(a: 1), (b: 2)}").obj) // エントリーのストリームでもよい
         assertEquals("{1:2;2:4;3:6}", run("{1 .. 3 | a => (a): a * 2}").obj) // エントリー列を返す式でもよい
+        assertEquals("{}", run(""" {} """).obj) // 空でもよい
     }
 
     @Test
