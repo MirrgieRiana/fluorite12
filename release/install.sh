4bd313a49e514e9ff356e10fbc7fd6607aee99f9
_ <tacticsrealize2@gmail.com>
2024-05-08 13:40:25 +0900

    Update install.sh

--- release/install.sh
+++ release/install.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
-git clone --single-branch --branch release --depth 1 https://github.com/MirrgieRiana/fluorite12.git || exit
+
+mkdir -p fluorite12 || exit
+
+if [ -d fluorite12/.git ]
+then
+  git -C fluorite12 fetch origin release
+  git -C fluorite12 reset --hard origin/release
+else
+  git clone --single-branch --branch release --depth 1 https://github.com/MirrgieRiana/fluorite12.git fluorite12 || exit
+fi
+
 destination=/usr/local/bin/flc
-[ -e "$destination" ] && { rm -i "$destination" || exit; }
+
+if [ -e "$destination" ]
+then
+  rm -i "$destination" || exit
+fi
+
 ln -s fluorite12/bin/linuxX64/flcReleaseExecutable/flc.kexe "$destination" || exit
