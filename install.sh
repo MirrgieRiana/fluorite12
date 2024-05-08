b4cb934eeab42fcfeeb1ff72f1e932e9e855f308
GitHub Action <action@github.com>
2024-05-08 05:06:16 +0000

    v0.1.8 - 776ccf6cb29ba0eb6d02cb8f0dfd2df3a67d5e3c

--- install.sh
+++ install.sh
@@ -4,17 +4,13 @@ mkdir -p fluorite12 || exit
 
 if [ -d fluorite12/.git ]
 then
-  git -C fluorite12 fetch origin release
-  git -C fluorite12 reset --hard origin/release
+  git -C fluorite12 fetch origin release || exit
+  git -C fluorite12 reset --hard origin/release || exit
 else
   git clone --single-branch --branch release --depth 1 https://github.com/MirrgieRiana/fluorite12.git fluorite12 || exit
 fi
 
 destination=/usr/local/bin/flc
 
-if [ -e "$destination" ]
-then
-  rm -i "$destination" || exit
-fi
-
+rm -f "$destination" || exit
 ln -s "$(pwd)"/fluorite12/bin/linuxX64/flcReleaseExecutable/flc.kexe "$destination" || exit
