ec729de3f554fc66a6b33b30020d28ec17727a5d
GitHub Action <action@github.com>
2024-05-08 12:09:11 +0000

    v0.1.14 - 34f4d5113f72b522f4ced4ec629da0c1646f5467

--- install.sh
+++ install.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
-mkdir -p fluorite12 || exit
-
-if [ -d fluorite12/.git ]
+# インストールディレクトリの判定
+if [ "${BASH_SOURCE[0]}" = "" ]
 then
-  git -C fluorite12 fetch origin release || exit
-  git -C fluorite12 reset --hard origin/release || exit
+  # パイプで渡された
+  SCRIPT_DIR=$(pwd)/fluorite12
 else
-  git clone --single-branch --branch release --depth 1 https://github.com/MirrgieRiana/fluorite12.git fluorite12 || exit
+  # ファイルを実行した
+  SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)
 fi
 
+# インストールディレクトリの準備
+mkdir -p "$SCRIPT_DIR" || exit
+
+# リポジトリを最新の状態に更新
+if [ -d "$SCRIPT_DIR"/.git ]
+then
+  git -C "$SCRIPT_DIR" fetch origin release || exit
+  git -C "$SCRIPT_DIR" reset --hard origin/release || exit
+else
+  git clone --single-branch --branch release --depth 1 https://github.com/MirrgieRiana/fluorite12.git "$SCRIPT_DIR" || exit
+fi
+
+# /usr/local/bin にインストール
 destination=/usr/local/bin/flc
 rm -f "$destination" || exit
-ln -s "$(pwd)"/fluorite12/bin/linuxX64/flcReleaseExecutable/flc.kexe "$destination" || exit
-
+ln -s "$SCRIPT_DIR"/bin/linuxX64/flcReleaseExecutable/flc.kexe "$destination" || exit
 destination=/usr/local/bin/flc-update
 rm -f "$destination" || exit
-ln -s "$(pwd)"/fluorite12/update.sh "$destination" || exit
+ln -s "$SCRIPT_DIR"/install.sh "$destination" || exit
