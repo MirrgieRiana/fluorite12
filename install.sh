bb83040d94ad018e1fdad6febdf287fc7d571afe
GitHub Action <action@github.com>
2024-05-08 04:16:04 +0000

    v0.1.4 - 0984c53a2479caa7d6c808aa1a72530c3ac5ad48

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+git clone --single-branch --branch release --depth 1 https://github.com/MirrgieRiana/fluorite12.git || exit
+destination=/usr/local/bin/flc
+[ -e "$destination" ] && rm -i "$destination" || exit
+ln -s fluorite12/bin/linuxX64/flcReleaseExecutable/flc.kexe "$destination" || exit
