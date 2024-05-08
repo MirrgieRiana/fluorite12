c8c79be364623e74eff949c36579b7e322dba6d8
_ <tacticsrealize2@gmail.com>
2024-05-08 14:57:11 +0900

    ADD: flc-update

---
+++ release/update.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+SCRIPT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+git -C "$SCRIPT_PATH" fetch origin release || exit
+git -C "$SCRIPT_PATH" reset --hard origin/release || exit
