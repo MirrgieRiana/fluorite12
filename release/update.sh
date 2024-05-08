0b50ee03b374220a46e0923129537c796a01efce
_ <tacticsrealize2@gmail.com>
2024-05-08 20:11:06 +0900

    Update update.sh

--- release/update.sh
+++ release/update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-SCRIPT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+SCRIPT_PATH=$(cd "$(dirname "$(readlinf -f "${BASH_SOURCE[0]}")")" && pwd)
 git -C "$SCRIPT_PATH" fetch origin release || exit
 git -C "$SCRIPT_PATH" reset --hard origin/release || exit
