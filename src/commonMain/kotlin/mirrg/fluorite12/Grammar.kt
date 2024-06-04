676df3bc0161302a56016b9d4b99a7414910f1e0
_ <tacticsrealize2@gmail.com>
2024-06-04 13:57:44 +0900

    FIX: && token

--- src/commonMain/kotlin/mirrg/fluorite12/Grammar.kt
+++ src/commonMain/kotlin/mirrg/fluorite12/Grammar.kt
@@ -257,7 +257,7 @@ class Fluorite12Grammar : Grammar<Node>() {
     val left: Parser<Node> by zeroOrMore(leftOperator) * right map { it.t1.foldRight(it.t2) { f, node -> f(node) } }
 
     val mul: Parser<Node> by leftAssociative(left, -s * (+asterisk or +slash or +(percent * percent) or +percent) * -b, ::infixNode)
-    val add: Parser<Node> by leftAssociative(mul, -s * (+plus or +(minus * -NotParser(greater)) or +ampersand) * -b, ::infixNode)
+    val add: Parser<Node> by leftAssociative(mul, -s * (+plus or +(minus * -NotParser(greater)) or +(ampersand * -NotParser(ampersand))) * -b, ::infixNode)
     val range: Parser<Node> by leftAssociative(add, -s * +(period * period) * -b, ::infixNode)
     val comparisonOperator: Parser<List<TokenMatch>> by OrCombinator(
         +(equal * equal), // ==
