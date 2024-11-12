b90482aec6e9cb6db1b045124db54cd16c618d27
_ <tacticsrealize2@gmail.com>
2024-11-12 17:38:50 +0900

    test: change: ArrayTest

--- src/commonTest/kotlin/mirrg/fluorite12/ArrayTest.kt
+++ src/commonTest/kotlin/mirrg/fluorite12/ArrayTest.kt
@@ -9,34 +9,60 @@ import kotlin.test.assertEquals
 class ArrayTest {
 
     @Test
-    fun arrayTest() = runTest {
-        assertEquals("[1]", run("[1]").array) // [ ] で配列を作れる
-        assertEquals("[1;2]", run("[1; 2]").array) // 要素は ; で区切ることができる
+    fun create() = runTest {
+        assertEquals("[1]", run("[1]").array) // 配列の生成
+        assertEquals("[1;2]", run("[1; 2]").array) // 複数の要素を持つ配列
         assertEquals("[1;2]", run("[; ; 1; ; 2; ; ]").array) // ; は無駄に大量にあってもよい
         assertEquals("[]", run("[; ]").array) // ; しかなくてもよい
-        assertEquals("[1;2]", run("[1, 2]").array) // 要素のストリームでもよい
-        assertEquals("[1;2;3]", run("[1, 2; 3]").array) // 要素のストリームと要素が混在してもよい
-        assertEquals("[1;2;3;4;5;6;7;8]", run("[1; 2..4; 0..2 | _ + 5; 8]").array) // 埋め込まれたストリームは自動的に展開される
+        assertEquals("[1;2]", run("[1, 2]").array) // ストリームは展開される
+        assertEquals("[1;2;3]", run("[1, 2; 3]").array) // ストリームと要素が混在してもよい
+        assertEquals("[1;2;3;4;5;6;7;8]", run("[1; 2..4; 0..2 | _ + 5; 8]").array) // 複雑なストリームの例
         assertEquals("[]", run("[]").array) // 空でもよい
     }
 
     @Test
-    fun arrayAssignmentTest() = runTest {
-        assertEquals("[10;99;30]", run("array := [10, 20, 30]; array(1) = 99; array").array) // 配列の要素に代入できる
+    fun assign() = runTest {
+        // 配列の要素への代入
+        """
+           array := [10, 20, 30]
+           array(1) = 99
+           array
+        """.let { assertEquals("[10;99;30]", run(it).array) }
     }
 
     @Test
-    fun bracketsAccessTest() = runTest {
-        assertEquals("1,2,3", run("[1; 2; 3]()").stream()) // array() でストリームにする
-        assertEquals(2, run("[1; 2; 3](1)").int) // array(index) で要素を得る
+    @Test
+    fun invoke() = runTest {
+        assertEquals("1,2,3", run("[1; 2; 3]()").stream()) // ストリーム化
+        assertEquals(2, run("[1; 2; 3](1)").int) // 関数呼び出しによる単一要素の取得
+        assertEquals(2, run("[1; 2; 3]('0.95')").int) // インデックスは数値化し、四捨五入される
+        assertEquals(FluoriteNull, run("[1; 2; 3](3)")) // 範囲外のインデックスは NULL が返る
         assertEquals(3, run("[1; 2; 3](-1)").int) // 負のインデックスは後ろから数える
-        assertEquals("3,3,1,2", run("[1; 2; 3](2, 2, 0, 1)").stream()) // array(indices) で要素のストリームを得る
+        assertEquals("3,3,1,2", run("[1; 2; 3](2, 2, 0, 1)").stream()) // インデックスのストリームは要素のストリームを返す
     }
 
     @Test
-    fun addTest() = runTest {
-        // TODO
-        assertEquals("[1;2;3;4]", run("[1; 2] + [3; 4]").array) // 配列の加算は配列同士を連結する
+    fun access() = runTest {
+        assertEquals(2, run("[1; 2; 3].1").int) // 要素アクセス
+        assertEquals(2, run("[1; 2; 3].'0.95'").int) // インデックスは数値化し、四捨五入される
+        assertEquals(FluoriteNull, run("[1; 2; 3].3")) // 範囲外のインデックスは NULL が返る
+        assertEquals(FluoriteNull, run("[1; 2; 3].(-1)")) // 負のインデックスも範囲外扱い
+    }
+
+    @Test
+    fun length() = runTest {
+        assertEquals(3, run("$#[1; 2; 3]").int) // 長さの取得
+        assertEquals(0, run("$#[]").int) // 空配列の長さは 0
+    }
+
+    @Test
+    fun plus() = runTest {
+        assertEquals("[1;2;3;4]", run("[1; 2] + [3; 4]").array) // 配列同士の連結
+    }
+
+    @Test
+    fun times() = runTest {
+        assertEquals("[1;2;1;2;1;2]", run("[1; 2] * 3").array) // 配列を繰り返した配列の取得
     }
 
 }
