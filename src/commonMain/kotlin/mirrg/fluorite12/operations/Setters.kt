fc4e6e06f49edb8de52bb5b35ef6f62a3eb21903
_ <tacticsrealize2@gmail.com>
2024-11-13 17:11:24 +0900

    FIX: ArrayItemSetter

--- src/commonMain/kotlin/mirrg/fluorite12/operations/Setters.kt
+++ src/commonMain/kotlin/mirrg/fluorite12/operations/Setters.kt
@@ -2,9 +2,9 @@ package mirrg.fluorite12.operations
 
 import mirrg.fluorite12.Environment
 import mirrg.fluorite12.FluoriteArray
-import mirrg.fluorite12.FluoriteInt
 import mirrg.fluorite12.FluoriteStream
 import mirrg.fluorite12.FluoriteValue
+import mirrg.fluorite12.toFluoriteNumber
 
 class VariableSetter(private val frameIndex: Int, private val variableIndex: Int) : Setter {
     override suspend fun evaluate(env: Environment): suspend (FluoriteValue) -> Unit {
@@ -19,7 +19,7 @@ class VariableSetter(private val frameIndex: Int, private val variableIndex: Int
 class ArrayItemSetter(private val arrayGetter: Getter, private val indexGetter: Getter) : Setter {
     override suspend fun evaluate(env: Environment): suspend (FluoriteValue) -> Unit {
         val array = arrayGetter.evaluate(env) as FluoriteArray
-        val index = (indexGetter.evaluate(env) as FluoriteInt).value
+        val index = indexGetter.evaluate(env).toFluoriteNumber().roundToInt()
         return {
             if (it is FluoriteStream) throw IllegalArgumentException("Stream assignment is not supported")
             array.values[index] = it
