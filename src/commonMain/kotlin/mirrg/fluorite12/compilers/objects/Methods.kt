16e960ed5aa3f22c598eb16a0036e1445fc45832
_ <tacticsrealize2@gmail.com>
2024-11-15 17:43:22 +0900

    refactor: move: FluoriteValue.*

--- src/commonMain/kotlin/mirrg/fluorite12/compilers/objects/Methods.kt
+++ src/commonMain/kotlin/mirrg/fluorite12/compilers/objects/Methods.kt
@@ -5,45 +5,6 @@ import mirrg.fluorite12.FormatterConversion
 import mirrg.fluorite12.FormatterFlag
 import mirrg.fluorite12.removeExponent
 
-fun FluoriteValue.instanceOf(clazz: FluoriteValue): Boolean {
-    var currentObject: FluoriteValue? = this
-    while (true) {
-        if (currentObject == null) return false
-        if (currentObject === clazz) return true
-        currentObject = currentObject.parent
-    }
-}
-
-fun FluoriteValue.getMethod(name: String): FluoriteValue? {
-    var currentObject = parent
-    while (true) {
-        if (currentObject == null) return null
-
-        val value = currentObject.map[name]
-        if (value != null) return value
-
-        currentObject = currentObject.parent
-    }
-}
-
-suspend fun FluoriteValue.callMethod(name: String, arguments: Array<FluoriteValue> = arrayOf()): FluoriteValue {
-    val method = this.getMethod(name) ?: throw RuntimeException("Method not found: $this::$name")
-    return if (method is FluoriteFunction) {
-        method.function(arrayOf(this, *arguments))
-    } else {
-        method.invoke(arrayOf(this, *arguments))
-    }
-}
-
-suspend fun FluoriteValue.invoke(arguments: Array<FluoriteValue>) = this.callMethod("INVOKE", arguments)
-suspend fun FluoriteValue.bind(arguments: Array<FluoriteValue>) = this.callMethod("BIND", arguments)
-suspend fun FluoriteValue.toJson() = this.callMethod("TO_JSON")
-suspend fun FluoriteValue.toFluoriteNumber(): FluoriteNumber = this.callMethod("TO_NUMBER").let { if (it is FluoriteNumber) it else it.toFluoriteNumber() }
-suspend fun FluoriteValue.toFluoriteString(): FluoriteString = this.callMethod("TO_STRING").let { if (it is FluoriteString) it else it.toFluoriteString() }
-suspend fun FluoriteValue.toFluoriteBoolean(): FluoriteBoolean = this.callMethod("TO_BOOLEAN").let { if (it is FluoriteBoolean) it else it.toFluoriteBoolean() }
-suspend fun FluoriteValue.toBoolean() = this.toFluoriteBoolean().value
-suspend fun FluoriteValue.contains(value: FluoriteValue) = this.callMethod("CONTAINS", arrayOf(value)).toFluoriteBoolean()
-
 suspend fun Formatter.format(value: FluoriteValue): String {
     val (sign, string) = if (this.conversion == FormatterConversion.STRING) {
         if (FormatterFlag.SIGNED in this.flags) throw RuntimeException("Invalid format: ${this.string}")
