d6c7b384f0d07323dc9be8fb1f384d43aa2295a6
_ <tacticsrealize2@gmail.com>
2024-12-18 09:54:47 +0900

    comment: Comparators

--- src/commonMain/kotlin/mirrg/fluorite12/operations/Comparators.kt
+++ src/commonMain/kotlin/mirrg/fluorite12/operations/Comparators.kt
@@ -6,31 +6,37 @@ import mirrg.fluorite12.compilers.objects.FluoriteValue
 import mirrg.fluorite12.compilers.objects.contains
 import mirrg.fluorite12.compilers.objects.instanceOf
 
+// TODO
 object EqualComparator : Comparator {
     override suspend fun evaluate(env: Environment): suspend (FluoriteValue, FluoriteValue) -> Boolean = { a, b -> a == b }
     override val code get() = "Equal"
 }
 
+// TODO
 object NotEqualComparator : Comparator {
     override suspend fun evaluate(env: Environment): suspend (FluoriteValue, FluoriteValue) -> Boolean = { a, b -> a != b }
     override val code get() = "NotEqual"
 }
 
+// TODO
 object GreaterComparator : Comparator {
     override suspend fun evaluate(env: Environment): suspend (FluoriteValue, FluoriteValue) -> Boolean = { a, b -> (a as FluoriteNumber).value.toDouble() > (b as FluoriteNumber).value.toDouble() }
     override val code get() = "Greater"
 }
 
+// TODO
 object LessComparator : Comparator {
     override suspend fun evaluate(env: Environment): suspend (FluoriteValue, FluoriteValue) -> Boolean = { a, b -> (a as FluoriteNumber).value.toDouble() < (b as FluoriteNumber).value.toDouble() }
     override val code get() = "Less"
 }
 
+// TODO
 object GreaterEqualComparator : Comparator {
     override suspend fun evaluate(env: Environment): suspend (FluoriteValue, FluoriteValue) -> Boolean = { a, b -> (a as FluoriteNumber).value.toDouble() >= (b as FluoriteNumber).value.toDouble() }
     override val code get() = "GreaterEqual"
 }
 
+// TODO
 object LessEqualComparator : Comparator {
     override suspend fun evaluate(env: Environment): suspend (FluoriteValue, FluoriteValue) -> Boolean = { a, b -> (a as FluoriteNumber).value.toDouble() <= (b as FluoriteNumber).value.toDouble() }
     override val code get() = "LessEqual"
