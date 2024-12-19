a441ed925223e239f657edf8deb441fe936425c2
_ <tacticsrealize2@gmail.com>
2024-12-19 11:30:03 +0900

    playground: build: add: logging

--- playground/compile.sh
+++ playground/compile.sh
@@ -3,11 +3,14 @@
 cd "$(dirname "$0")" || exit
 
 # Clean build directory
+echo "[INFO ] Cleaning build directory..."
 rm -rf build || exit
 mkdir -p build || exit
 
 # Compile editor
+echo "[INFO ] Installing npm packages..."
 npm install || exit
+echo "[INFO ] Compiling editor..."
 mkdir -p build/out || exit
 node_modules/.bin/rollup \
   editor.mjs \
@@ -17,7 +20,9 @@ node_modules/.bin/rollup \
   || exit
 
 # Copy html files
+echo "[INFO ] Copying html files..."
 rsync -av src/ build/out || exit
 
 # Copy fluorite12
+echo "[INFO ] Copying fluorite12..."
 rsync -av ../build/distributions/ build/out || exit
