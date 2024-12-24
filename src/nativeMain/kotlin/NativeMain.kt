ecc6762c7dc83d09583e572934e65276486c83dd
_ <tacticsrealize2@gmail.com>
2024-12-24 15:05:49 +0900

    ADD: cli: -h

--- src/nativeMain/kotlin/NativeMain.kt
+++ src/nativeMain/kotlin/NativeMain.kt
@@ -5,14 +5,59 @@ import mirrg.fluorite12.compilers.objects.collect
 import mirrg.fluorite12.compilers.objects.toFluoriteString
 import mirrg.fluorite12.mounts.createCommonMount
 
-fun main(args: Array<String>) = runBlocking {
+fun main(args: Array<String>) {
+    val list = args.toMutableList()
+    var src: String? = null
+    val arguments = mutableListOf<String>()
+
+    try {
+        run {
+            while (true) {
+
+                if (list.firstOrNull() == "--") {
+                    list.removeFirst()
+
+                    if (list.isEmpty()) throw ShowUsage
+
+                    src = list.removeFirst()
+                    arguments += list
+                    list.clear()
+                    return@run
+                }
+
+                if (list.firstOrNull() == "-h") throw ShowUsage
+                if (list.firstOrNull() == "--help") throw ShowUsage
+
+                if (list.isEmpty()) throw ShowUsage
+
+                src = list.removeFirst()
+                arguments += list
+                list.clear()
+                return@run
+            }
+        }
+    } catch (_: ShowUsage) {
+        println("Usage: flc [-h|--help] [-q] [--] <code> <arguments...>")
+        println("Options:")
+        println("  -h, --help               Show this help")
+        return
+    }
+
+    runBlocking {
+        main(src!!, arguments)
+    }
+}
+
+private object ShowUsage : Throwable()
+
+suspend fun main(src: String, arguments: List<String>) {
     val evaluator = Evaluator()
     val defaultBuiltinMounts = listOf(
         createCommonMount(),
-        createNativeMount(args),
+        createNativeMount(arguments),
     )
     evaluator.defineMounts(defaultBuiltinMounts)
-    val result = evaluator.get(args[0])
+    val result = evaluator.get(src)
     if (result is FluoriteStream) {
         result.collect {
             println(it.toFluoriteString())
