b0a6f48f7ebba7e84df5d93bdd3175dbfcd91e6e
_ <tacticsrealize2@gmail.com>
2025-05-22 00:23:13 +0900

    build: add: openjdk17

--- codex.setup.bash
+++ codex.setup.bash
@@ -3,3 +3,5 @@
 cd "$(dirname "$0")" || exit
 
 bash gradlew --no-daemon --refresh-dependencies downloadDependencies
+
+wget -O openjdk17.tar.gz "https://api.adoptium.net/v3/binary/latest/17/ga/linux/x64/jdk/hotspot/normal/eclipse?project=jdk"
