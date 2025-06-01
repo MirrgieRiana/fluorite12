463a6dc1949dc792486e2239f17bb861c9704d47
_ <tacticsrealize2@gmail.com>
2025-06-01 13:13:31 +0900

    ADD: CSV, CSVD

--- src/commonMain/kotlin/mirrg/fluorite12/mounts/CommonMount.kt
+++ src/commonMain/kotlin/mirrg/fluorite12/mounts/CommonMount.kt
@@ -330,6 +330,177 @@ fun createCommonMount(): Map<String, FluoriteValue> {
             val json = arguments[0]
             json.toFluoriteValueAsJson()
         },
+        "CSV" to FluoriteFunction { arguments ->
+            fun usage(): Nothing = usage("""CSV(["separator": separator: STRING; ]["quote": quote: STRING; ]value: ARRAY<STRING> | STREAM<ARRAY<STRING>>): STRING | STREAM<STRING>""")
+            if (arguments.isEmpty()) usage()
+            val parameters = arguments.dropLast(1)
+                .associate {
+                    val array = it as? FluoriteArray ?: usage()
+                    if (array.values.size != 2) usage()
+                    val key = array.values[0] as? FluoriteString ?: usage()
+                    val value = array.values[1]
+                    key.value to value
+                }
+                .toMutableMap()
+            val separator = parameters.remove("separator")?.let {
+                val string = it.toFluoriteString().value
+                check(string.length == 1)
+                string.first()
+            } ?: ','
+            val quote = parameters.remove("quote")?.let {
+                val string = it.toFluoriteString().value
+                check(string.length == 1)
+                string.first()
+            } ?: '"'
+            if (parameters.isNotEmpty()) usage()
+            val value = arguments.last()
+
+            suspend fun toCsv(value: FluoriteValue): FluoriteString {
+                val sb = StringBuilder()
+                (value as FluoriteArray).values.forEachIndexed { index, value ->
+                    if (index > 0) sb.append(separator)
+                    val string = value.toFluoriteString().value
+                    if (string.isEmpty()) return@forEachIndexed
+                    val needQuote = run {
+                        val first = string.first()
+                        if (first == ' ') return@run true
+                        if (first == '\t') return@run true
+                        val last = string.last()
+                        if (last == ' ') return@run true
+                        if (last == '\t') return@run true
+                        if (separator in string) return@run true
+                        if (quote in string) return@run true
+                        if ('\r' in string) return@run true
+                        if ('\n' in string) return@run true
+                        false
+                    }
+                    if (needQuote) {
+                        sb.append(quote)
+                        sb.append(string.replace("$quote", "$quote$quote"))
+                        sb.append(quote)
+                    } else {
+                        sb.append(string)
+                    }
+                }
+                return sb.toString().toFluoriteString()
+            }
+
+            if (value is FluoriteStream) {
+                FluoriteStream {
+                    value.collect {
+                        emit(toCsv(it))
+                    }
+                }
+            } else {
+                toCsv(value)
+            }
+        },
+        "CSVD" to FluoriteFunction { arguments ->
+            fun usage(): Nothing = usage("""CSVD(["separator": separator: STRING; ]["quote": quote: STRING; ]csv: STRING | STREAM<STRING>): ARRAY<STRING> | STREAM<ARRAY<STRING>>""")
+            if (arguments.isEmpty()) usage()
+            val parameters = arguments.dropLast(1)
+                .associate {
+                    val array = it as? FluoriteArray ?: usage()
+                    if (array.values.size != 2) usage()
+                    val key = array.values[0] as? FluoriteString ?: usage()
+                    val value = array.values[1]
+                    key.value to value
+                }
+                .toMutableMap()
+            val separator = parameters.remove("separator")?.let {
+                val string = it.toFluoriteString().value
+                check(string.length == 1)
+                string.first()
+            } ?: ','
+            val quote = parameters.remove("quote")?.let {
+                val string = it.toFluoriteString().value
+                check(string.length == 1)
+                string.first()
+            } ?: '"'
+            if (parameters.isNotEmpty()) usage()
+            val value = arguments.last()
+
+            suspend fun fromCsv(csv: FluoriteValue): FluoriteArray {
+                val list = mutableListOf<FluoriteValue>()
+                val sb = StringBuilder()
+                val quoted = mutableListOf<Boolean>()
+
+                val STATE_NOT_QUOTED = 0
+                val STATE_QUOTED = 1
+                val STATE_AFTER_QUOTED = 2
+                var state = STATE_NOT_QUOTED
+
+                fun flush() {
+                    var head = 0
+                    while (true) {
+                        if (head >= sb.length) break
+                        if (quoted[head]) break
+                        if (sb[head] != ' ' && sb[head] != '\t') break
+                        head++
+                    }
+
+                    var tail = sb.length - 1
+                    while (true) {
+                        if (tail + 1 <= head) break
+                        if (quoted[tail]) break
+                        if (sb[tail] != ' ' && sb[tail] != '\t') break
+                        tail--
+                    }
+
+                    list += sb.substring(head, tail + 1).toFluoriteString()
+                    sb.clear()
+                    quoted.clear()
+                }
+
+                csv.toFluoriteString().value.forEach { ch ->
+                    if (state == STATE_NOT_QUOTED) {
+                        if (ch == quote) {
+                            state = STATE_QUOTED
+                        } else if (ch == separator) {
+                            flush()
+                        } else {
+                            sb.append(ch)
+                            quoted += false
+                        }
+                    } else if (state == STATE_QUOTED) {
+                        if (ch == quote) {
+                            state = STATE_AFTER_QUOTED
+                        } else {
+                            sb.append(ch)
+                            quoted += true
+                        }
+                    } else if (state == STATE_AFTER_QUOTED) {
+                        if (ch == quote) {
+                            sb.append(ch)
+                            quoted += false
+                            state = STATE_QUOTED
+                        } else if (ch == separator) {
+                            flush()
+                            state = STATE_NOT_QUOTED
+                        } else {
+                            sb.append(ch)
+                            quoted += false
+                            state = STATE_NOT_QUOTED
+                        }
+                    } else {
+                        throw AssertionError()
+                    }
+                }
+                flush()
+
+                return FluoriteArray(list)
+            }
+
+            if (value is FluoriteStream) {
+                FluoriteStream {
+                    value.collect {
+                        emit(fromCsv(it))
+                    }
+                }
+            } else {
+                fromCsv(value)
+            }
+        },
         "GENERATE" to FluoriteFunction { arguments ->
             if (arguments.size != 1) usage("GENERATE(generator: (yield: (value: VALUE) -> NULL) -> NULL | STREAM): STREAM<VALUE>")
             val generator = arguments[0]
