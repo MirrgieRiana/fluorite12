5485037e395a5457593d4fc6c7ab1d01dab1985b
_ <tacticsrealize2@gmail.com>
2025-11-14 15:26:26 +0900

    test: refactor: move: mathTest

---
+++ src/commonTest/kotlin/MathTest.kt
@@ -0,0 +1,35 @@
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MathTest {
+
+    @Test
+    fun mathTest() = runTest {
+        assertEquals(3.141592653589793, eval("MATH.PI").double, 0.001)
+        assertEquals(2.718281828459045, eval("MATH.E").double, 0.001)
+
+        assertEquals(3.141592653589793, eval("PI").double, 0.001)
+
+        // SQRT
+        assertEquals(10.0, eval("SQRT(100)").double, 0.001)
+
+        // SIN
+        assertEquals(0.0, eval("SIN(0)").double, 0.001)
+        assertEquals(1.0, eval("SIN(PI / 2)").double, 0.001)
+
+        // COS
+        assertEquals(1.0, eval("COS(0)").double, 0.001)
+        assertEquals(-1.0, eval("COS(PI)").double, 0.001)
+
+        // TAN
+        assertEquals(1.0, eval("TAN(PI / 4)").double, 0.001)
+
+        // LOG (自然対数)
+        assertEquals(1.0, eval("LOG(MATH.E)").double, 0.001)
+    }
+
+}
+
