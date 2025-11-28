39944cd5e1e8ffec199300ec15d82f2d06c58de2
_ <tacticsrealize2@gmail.com>
2025-11-28 12:12:08 +0900

    test: rerfactor: extract: FunctionTest

--- src/commonTest/kotlin/Fluorite12Test.kt
+++ src/commonTest/kotlin/Fluorite12Test.kt
@@ -722,29 +722,6 @@ class Fluorite12Test {
     }
 
     @Test
-    fun lambdaTest() = runTest {
-        assertEquals(10, eval("((a) -> a)(10)").int) // (a) -> b で関数を作り、 f(a) で実行する
-        assertEquals(12, eval("((a; b) -> a * b)(3; 4)").int) // ; で引数を複数取れる
-        assertEquals(12, eval("((a, b) -> a * b)(3; 4)").int) // ラムダ引数は , で区切ってもよい
-        assertEquals(10, eval("(() -> 10)()").int) // () で引数を無しにできる
-        assertEquals(10, eval("(a -> a)(10)").int) // 引数がある場合、 ( ) は省略してもよい
-        assertEquals(12, eval("(a, b -> a * b)(3; 4)").int) // 引数が複数の場合も ( ) を省略できる
-        assertEquals(12, eval("((, a, , b, ) -> a * b)(3; 4)").int) // 区切り文字を余計に書いてもよい
-        assertEquals(10, eval("(, -> 10)()").int) // , 1個でも引数無しを表せる
-
-        assertEquals("[1;2;3;4]", eval("(s -> &[s])(1, 2, 3, 4)").string) // 引数で , を使うとストリームを渡せる
-
-        assertEquals("[1;2;3;4;5]", eval("(() -> &__)(1; 2; 3; 4; 5)").string) // __ で引数を配列で受け取れる
-        assertEquals("[4;5;6]", eval("(() -> &[__.1])(1 .. 3; 4 .. 6; 7 .. 9)").string) // 引数列ではストリームを展開しない
-
-        assertEquals(120, eval("f := n -> n == 0 ? 1 : n * f(n - 1); f(5)").int) // 再帰関数の例
-        assertEquals(120, eval("(f -> f(f))(f -> n -> n == 0 ? 1 : n * f(f)(n - 1))(5)").int) // 複雑なラムダ計算の例
-
-        // 同じ関数を再帰的に2度呼び出した場合に、関数のフレームが分離されているかどうかのテスト
-        assertEquals("[2;1;2]", eval("f := n -> n == 1 ? 1 : [n, f(1), n]; f(2)").array())
-    }
-
-    @Test
     fun methodTest() = runTest {
         assertEquals(10, eval("{method: () -> 10}{}::method()").int) // a::b() でaのbを呼び出せる
         assertEquals(10, eval("{method: this -> this.a}{a: 10}::method()").int) // メソッド関数は最初の引数にthisを受け取る
@@ -773,15 +750,6 @@ class Fluorite12Test {
         """.let { assertEquals("[1;a]", eval(it).array()) }
 
         assertEquals(6, eval("mul := a, b -> a * b; f := 2::(mul); f(3)").int) // 関数へのメソッド参照
-    }
-
-    @Test
-    fun bindTest() = runTest {
-        assertEquals("12", eval("(a, b -> a & b)[1](2)").string) // [ ] で関数に部分適用できる
-        assertEquals("12", eval("(a, b -> a & b)[1; 2]()").string) // [ ] の中に複数の引数があってもよい
-        assertEquals("12", eval("(a, b -> a & b)[](1; 2)").string) // [ ] の中が空でもよい
-        assertEquals("12", eval("(a, b -> a & b)[1][2]()").string) // [ ] を連続して書いてもよい
-        assertEquals("12", eval("{m: _, a, b -> a & b}{}::m[1](2)").string) // メソッド呼び出しにも使用できる
     }
 
     @Test
@@ -1064,14 +1032,6 @@ class Fluorite12Test {
     }
 
     @Test
-    fun infixFunction() = runTest {
-        assertEquals(5, eval("add := a, b -> a + b; 2 add 3").int) // identifierを使った中置関数呼び出し
-        assertEquals(5, eval("add := a, b -> a + b; 2 `add` 3").int) // 引用符付きでもよい
-        assertEquals(true, eval("lt := a, b -> a < b; 2  lt 3").boolean) // 論理値を返す中置換数
-        assertEquals(false, eval("lt := a, b -> a < b; 2 !lt 3").boolean) // 否定中置換数
-    }
-
-    @Test
     fun spaceship() = runTest {
         // 数値
         assertEquals(-1, eval("1 <=> 2").int) // <=> は左辺が小さい場合は-1
@@ -1141,67 +1101,6 @@ class Fluorite12Test {
         // ストリームの通常メソッド呼び出しは各要素のメソッド呼び出しの結合になる
         assertEquals("caa,aca,aac", eval(" 'baa', 'aba', 'aab'  | _::replace('b'; 'c')").stream())
         assertEquals("caa,aca,aac", eval("('baa', 'aba', 'aab')    ::replace('b'; 'c')").stream())
-    }
-
-    @Test
-    fun arrowInvoke() = runTest {
-        val evaluator = Evaluator()
-        evaluator.defineMounts(createCommonMounts())
-
-        // _::_ でフォールバックメソッドを定義する
-        """
-            register := listener -> listener(23)
-
-            Obj := {
-                register: this, listener -> listener(this.x + 3)
-            }
-            obj := Obj{x: 20}
-        """.let { evaluator.run(it) }
-
-        assertEquals(123, evaluator.get("register ( event => 100 + event )").int) // クロージャ付き関数呼び出し
-        assertEquals(123, evaluator.get("register [ event => 100 + event ]()").int) // クロージャ付き関数の部分適用
-        assertEquals(123, evaluator.get("obj::register ( event => 100 + event )").int) // クロージャ付きメソッド呼び出し
-        assertEquals(123, evaluator.get("obj::register [ event => 100 + event ]()").int) // クロージャ付きメソッドの部分適用
-
-        assertEquals(123, evaluator.get("register ( event => 9; 9; 9; 100 + event )").int) // クロージャは ; を文の区切りとして解釈する
-    }
-
-    @Test
-    fun callFunction() = runTest {
-        assertEquals(6, eval("CALL(a, b -> a * b; [2; 3])").int) // 関数の呼び出し
-        assertEquals(123, eval("CALL(() -> 123; [])").int) // 空の引数
-        assertEquals(6, eval("CALL({m: a, b -> a.v * b}{v: 2}::m; [3])").int) // メソッド参照の呼び出し
-    }
-
-    @Test
-    fun setCallTest() = runTest {
-
-        // 代入呼び出しができる
-        """
-            value := NULL
-            function := new -> value = new
-            function() = 123
-            value
-        """.let { assertEquals(123, eval(it).int) }
-
-        // 代入値は引数列の最後に受け取る
-        """
-            value := NULL
-            function := a, new -> value = a + new
-            function(100) = 23
-            value
-        """.let { assertEquals(123, eval(it).int) }
-
-        // メソッドのオーバーライドが可能
-        """
-            value := NULL
-            function := {
-                `_()=_`: this, new -> value = new
-            }{}
-            function() = 123
-            value
-        """.let { assertEquals(123, eval(it).int) }
-
     }
 
     @Test
---
+++ src/commonTest/kotlin/FunctionTest.kt
@@ -0,0 +1,112 @@
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import mirrg.fluorite12.Evaluator
+import mirrg.fluorite12.mounts.createCommonMounts
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class FunctionTest {
+
+    @Test
+    fun lambda() = runTest {
+        assertEquals(10, eval("((a) -> a)(10)").int) // (a) -> b で関数を作り、 f(a) で実行する
+        assertEquals(12, eval("((a; b) -> a * b)(3; 4)").int) // ; で引数を複数取れる
+        assertEquals(12, eval("((a, b) -> a * b)(3; 4)").int) // ラムダ引数は , で区切ってもよい
+        assertEquals(10, eval("(() -> 10)()").int) // () で引数を無しにできる
+        assertEquals(10, eval("(a -> a)(10)").int) // 引数がある場合、 ( ) は省略してもよい
+        assertEquals(12, eval("(a, b -> a * b)(3; 4)").int) // 引数が複数の場合も ( ) を省略できる
+        assertEquals(12, eval("((, a, , b, ) -> a * b)(3; 4)").int) // 区切り文字を余計に書いてもよい
+        assertEquals(10, eval("(, -> 10)()").int) // , 1個でも引数無しを表せる
+
+        assertEquals("[1;2;3;4]", eval("(s -> &[s])(1, 2, 3, 4)").string) // 引数で , を使うとストリームを渡せる
+
+        assertEquals("[1;2;3;4;5]", eval("(() -> &__)(1; 2; 3; 4; 5)").string) // __ で引数を配列で受け取れる
+        assertEquals("[4;5;6]", eval("(() -> &[__.1])(1 .. 3; 4 .. 6; 7 .. 9)").string) // 引数列ではストリームを展開しない
+
+        assertEquals(120, eval("f := n -> n == 0 ? 1 : n * f(n - 1); f(5)").int) // 再帰関数の例
+        assertEquals(120, eval("(f -> f(f))(f -> n -> n == 0 ? 1 : n * f(f)(n - 1))(5)").int) // 複雑なラムダ計算の例
+
+        // 同じ関数を再帰的に2度呼び出した場合に、関数のフレームが分離されているかどうかのテスト
+        assertEquals("[2;1;2]", eval("f := n -> n == 1 ? 1 : [n, f(1), n]; f(2)").array())
+    }
+
+    @Test
+    fun bind() = runTest {
+        assertEquals("12", eval("(a, b -> a & b)[1](2)").string) // [ ] で関数に部分適用できる
+        assertEquals("12", eval("(a, b -> a & b)[1; 2]()").string) // [ ] の中に複数の引数があってもよい
+        assertEquals("12", eval("(a, b -> a & b)[](1; 2)").string) // [ ] の中が空でもよい
+        assertEquals("12", eval("(a, b -> a & b)[1][2]()").string) // [ ] を連続して書いてもよい
+        assertEquals("12", eval("{m: _, a, b -> a & b}{}::m[1](2)").string) // メソッド呼び出しにも使用できる
+    }
+
+    @Test
+    fun infixCall() = runTest {
+        assertEquals(5, eval("add := a, b -> a + b; 2 add 3").int) // identifierを使った中置関数呼び出し
+        assertEquals(5, eval("add := a, b -> a + b; 2 `add` 3").int) // 引用符付きでもよい
+        assertEquals(true, eval("lt := a, b -> a < b; 2  lt 3").boolean) // 論理値を返す中置換数
+        assertEquals(false, eval("lt := a, b -> a < b; 2 !lt 3").boolean) // 否定中置換数
+    }
+
+    @Test
+    fun arrowCall() = runTest {
+        val evaluator = Evaluator()
+        evaluator.defineMounts(createCommonMounts())
+
+        // _::_ でフォールバックメソッドを定義する
+        """
+            register := listener -> listener(23)
+
+            Obj := {
+                register: this, listener -> listener(this.x + 3)
+            }
+            obj := Obj{x: 20}
+        """.let { evaluator.run(it) }
+
+        assertEquals(123, evaluator.get("register ( event => 100 + event )").int) // クロージャ付き関数呼び出し
+        assertEquals(123, evaluator.get("register [ event => 100 + event ]()").int) // クロージャ付き関数の部分適用
+        assertEquals(123, evaluator.get("obj::register ( event => 100 + event )").int) // クロージャ付きメソッド呼び出し
+        assertEquals(123, evaluator.get("obj::register [ event => 100 + event ]()").int) // クロージャ付きメソッドの部分適用
+
+        assertEquals(123, evaluator.get("register ( event => 9; 9; 9; 100 + event )").int) // クロージャは ; を文の区切りとして解釈する
+    }
+
+    @Test
+    fun call() = runTest {
+        assertEquals(6, eval("CALL(a, b -> a * b; [2; 3])").int) // 関数の呼び出し
+        assertEquals(123, eval("CALL(() -> 123; [])").int) // 空の引数
+        assertEquals(6, eval("CALL({m: a, b -> a.v * b}{v: 2}::m; [3])").int) // メソッド参照の呼び出し
+    }
+
+    @Test
+    fun setCall() = runTest {
+
+        // 代入呼び出しができる
+        """
+            value := NULL
+            function := new -> value = new
+            function() = 123
+            value
+        """.let { assertEquals(123, eval(it).int) }
+
+        // 代入値は引数列の最後に受け取る
+        """
+            value := NULL
+            function := a, new -> value = a + new
+            function(100) = 23
+            value
+        """.let { assertEquals(123, eval(it).int) }
+
+        // メソッドのオーバーライドが可能
+        """
+            value := NULL
+            function := {
+                `_()=_`: this, new -> value = new
+            }{}
+            function() = 123
+            value
+        """.let { assertEquals(123, eval(it).int) }
+
+    }
+
+}
