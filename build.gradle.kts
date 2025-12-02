c0c68bbf68c0e21afbc41d185557a3a149412122
_ <tacticsrealize2@gmail.com>
2025-12-03 08:53:20 +0900

    build: use: kotlin: 1.6 -> 1.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink
 
 plugins {
-    kotlin("multiplatform") version "1.6.21"
-    kotlin("plugin.serialization") version "1.6.21"
+    kotlin("multiplatform") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
     id("com.dorongold.task-tree") version "2.1.1"
     id("build-logic")
 }
@@ -51,10 +51,10 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
                 implementation("com.squareup.okio:okio:3.2.0")
                 implementation("com.ionspin.kotlin:bignum:0.3.6")
-                implementation("mirrg.kotlin:mirrg.kotlin.helium-kotlin-1-6:4.0.1")
+                implementation("mirrg.kotlin:mirrg.kotlin.helium-kotlin-1-7:4.0.1")
                 compileOnly(kotlin("test")) // ここにも書かないとなぜかIDEAが認識しない
             }
         }
